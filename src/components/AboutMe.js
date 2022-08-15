1d9315ce71050838b09afe81cfb626777415f6a6
Kadika <dryan10101@gmail.com>
2022-08-15 16:54:40 -0700

    finished about me section

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -18,6 +18,11 @@ const styles = {
         paddingRight: 10,
         borderRightStyle: 'solid',
         width: '15%',
+    },
+    main: {
+      width: '85%',
+      padding: '0px 30px',
+      color: colors.frost,
     }
 }
 
@@ -27,6 +32,15 @@ function AboutMe() {
       <div style={styles.bigSideWords}>
         About Me
       </div>
+      <div style={styles.main}>
+            <p>
+                Hello!  My name is David Ryan.  I'm currently studying at the UCSD Extension Coding Bootcamp to learn coding.  I graduate from that in September 2022.  Welcome to my portfolio page.  I'm a problem solver at heart; whenever I see a problem all I can do is think about ways to solve it.  That's why I got interested in coding.  It's so much fun.  I get to build super interesting and challenging websites and tools.
+            </p>
+            <br></br>
+            <p>
+                Here you'll have access to see some of my projects as well as info about how to contact me.  I'm a very responsive person when to comes to contacting me.  I check my email roughly twice a day at minimum and I answer texts as soon as I read them (usually within an hour or two).  I do not answer phone calls from unknown numbers unless I'm expecting a call.  Feel free to reach out to me whenever.
+            </p>
+        </div>
     </section>
   );
 }
