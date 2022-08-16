ceb8abd4c3c6d40b7b75bc4305b7bc72afde7434
Kadika <dryan10101@gmail.com>
2022-08-16 12:16:56 -0700

    restarted essentially.  Copy pasted the old html file into All component and imported the old stylesheet as well instead of doing inline styles because it was overcomplicating the project.  Added new color scheme, not fully in use yet.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Top from "./components/Top";
-import AboutMe from "./components/AboutMe";
-import Work from "./components/Work";
-import Spacer from "./components/Spacer";
+import All from "./components/All";
 
 function App() {
   return (
     <div>
-      <Top />
-      <Spacer />
-      <AboutMe />
-      <Spacer />
-      <Work />
-      <Spacer />
+      <All />
     </div>
   );
 }
--- src/components/assets/colors.js
+++
@@ -1,9 +0,0 @@
-const colors = {
-    black: '#000000',
-    lavender: '#D3B1D3',
-    rust: '#B95F27',
-    red: '#BE0E0E',
-    frost: '#F5F5F5'
-};
-
-module.exports = colors;
