8caaf6d5285da944fd1dc41e86c7546b87c80338
Kadika <dryan10101@gmail.com>
2022-08-17 15:05:01 -0700

    set up Nav.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,15 +4,16 @@ import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
+import '../styles/stylesheet.css';
 
 export default function Main() {
     const [currentPage, setCurrentPage] = useState('About');
 
     const renderPage = () => {
-        if (currentPage === 'About') {return <About />;};
+        if (currentPage === 'Resume') {return <Resume />;};
         if (currentPage === 'Portfolio') {return <Portfolio />;};
         if (currentPage === 'Contact') {return <Contact />;};
-        return <Resume />;
+        return <About />;
     };
 
     const handlePageChange = (page) => {setCurrentPage(page)};
---
+++ src/components/Nav.js
@@ -0,0 +1,49 @@
+import React from 'react';
+
+export default function Nav({ currentPage, handlePageChange }) {
+    return (
+        <section id="titleContainer" className="flexy toSides">
+        <div id="title">
+            Portfolio: David Ryan
+        </div>
+        <div className="flexy">
+            <div className="titleLink">
+                <a
+                className={currentPage === 'About' ? 'titleACurrent' : 'titleA'}
+                href="#aboutMe"
+                onClick={() => handlePageChange('About')}
+                >
+                    About Me
+                </a>
+            </div>
+            <div className="titleLink">
+                <a
+                className={currentPage === 'Portfolio' ? 'titleACurrent' : 'titleA'}
+                href="#portfolio"
+                onClick={() => handlePageChange('Portfolio')}
+                >
+                    Portfolio
+                </a>
+            </div>
+            <div className="titleLink">
+                <a
+                className={currentPage === 'Contact' ? 'titleACurrent' : 'titleA'}
+                href="#contact"
+                onClick={() => handlePageChange('Contact')}
+                >
+                    Contact
+                </a>
+            </div>
+            <div className="titleLink">
+                <a
+                className={currentPage === 'Resume' ? 'titleACurrent' : 'titleA'}
+                href="#resume"
+                onClick={() => handlePageChange('Resume')}
+                >
+                    Resume
+                </a>
+            </div>
+        </div>
+    </section>
+    );
+};
