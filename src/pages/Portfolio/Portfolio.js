dee15e7c24364a0100e305324c7d02255e9e8131
Kadika <dryan10101@gmail.com>
2022-09-14 15:59:22 -0700

    updated portfolio pieces

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -10,7 +10,12 @@ export default function Portfolio() {
                 </h3>
             </div>
             <div className="flexy fullContainer mainStuffWork">
-                <Project />
+                <Project num="1" title="PetConnect 2.0" subtitle="Bootcamp Final Project - Fully Deployed React Website" link="https://thepetconnection.herokuapp.com/" />
+                <Project num="2" title="Book Search Engine" subtitle="My first React app" link="https://github.com/Kadika38/Book-Search-Engine-David-Ryan" />
+                <Project num="3" title="JATE" subtitle="PWA Text Editor" link="https://jate-david-ryan.herokuapp.com/" />
+                <Project num="4" title="PetConnect 1.0" subtitle="Bootcamp Project - Deployed SQL and Handlebars Website" link="https://thepetconnection.herokuapp.com/" />
+                <Project num="5" title="Social Network API" subtitle="Back-end only exemplifying MongoDB and Mongoose" link="https://github.com/Kadika38/Social-Network-API-David-Ryan" />
+                <Project num="6" title="Tech Blog" subtitle="Simple blog site using express, handlebars, and sequelize" link="https://tech-blog-david-ryan.herokuapp.com/" />
             </div>
         </section>
     );
