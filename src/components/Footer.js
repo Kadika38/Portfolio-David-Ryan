d24b22a8480f7761a126a8fb344a42e2d45913e8
Kadika <dryan10101@gmail.com>
2022-09-14 14:55:56 -0700

    reworked setup because it was bad; added Footer file (unfinished)

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function Footer() {
+    return (
+        <section id="footerContainer" className="flexy equally">
+            
+        </section>
+    )
+}
