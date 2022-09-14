cec1dbb9161799aeec4dad216570347efac1ae5d
Kadika <dryan10101@gmail.com>
2022-09-14 16:11:38 -0700

    ~50% done with Resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,33 @@
 import React from 'react';
 
+//TODO: fix spacing by sectioning into "fullContainer" divs
+
 export default function Resume() {
     return (
-        <section>
-            <div>
-                hi im a resume sec
+        <section className="flexy bodySec">
+            <div className="flexy mainStuff">
+                <h3>
+                    Resume
+                </h3>
+                <br></br>
+                <p>
+                    Download my 
+                    <span> resume</span>
+                </p>
+                <br></br>
+                <h4>Front-end Proficiencies</h4>
+                <ul>
+                    <li>HTML</li>
+                    <li>CSS</li>
+                    <li>JavaScript</li>
+                </ul>
+                <br></br>
+                <h4>Back-end Proficiencies</h4>
+                <ul>
+                    <li>APIs</li>
+                    <li>Node</li>
+                    <li>Express</li>
+                </ul>
             </div>
         </section>
     );
