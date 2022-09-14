d8627444f8b9bbfb08c45f44ffbbc1a664516970
Kadika <dryan10101@gmail.com>
2022-09-14 15:09:59 -0700

    finished Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,9 +2,11 @@ import React from 'react';
 
 export default function Footer() {
     return (
-        <section id="footerContainer" className="flexy equally">
-            <a className='footerA'>LinkedIn</a>
-            <a className='footerA'>GitHub</a>
+        <section id="footerContainer" className="flexy center">
+            <div id='footerLinkContainer' className='flexy equally'>
+                <a className='footerA' href='https://www.linkedin.com/in/david-ryan-305860183/'>LinkedIn</a>
+                <a className='footerA' href='https://github.com/Kadika38'>GitHub</a>
+            </div>
         </section>
     )
 }
