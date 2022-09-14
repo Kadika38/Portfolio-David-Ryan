7019d2b43bab76babef7921847d120d80cb17440
Kadika <dryan10101@gmail.com>
2022-09-14 14:58:14 -0700

    Footer set up without styling

--- src/Main.js
+++ src/Main.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
-import Nav from './Nav';
+import Nav from './components/Nav';
 import About from './pages/About';
 import Portfolio from './pages/Portfolio';
 import Contact from './pages/Contact';
 import Resume from './pages/Resume';
 import '../styles/stylesheet.css';
+import Footer from './components/Footer';
 
 export default function Main() {
     const [currentPage, setCurrentPage] = useState('About');
@@ -22,6 +23,7 @@ export default function Main() {
         <div>
             <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
             {renderPage()}
+            <Footer />
         </div>
     );
 };
--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,8 @@ import React from 'react';
 export default function Footer() {
     return (
         <section id="footerContainer" className="flexy equally">
-            
+            <a className='footerA'>LinkedIn</a>
+            <a className='footerA'>GitHub</a>
         </section>
     )
 }
