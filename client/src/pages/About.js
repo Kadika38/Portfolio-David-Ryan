3737e652a6e6a8d865ce4ab58dad79d3a4e2f589
Kadika <dryan10101@gmail.com>
2022-09-22 17:01:04 -0700

    updated About.js"

--- client/src/pages/About.js
+++ client/src/pages/About.js
@@ -12,15 +12,15 @@ export default function About() {
                 <br/>
                 <br/>
                 <p>
-                    Hello!  My name is David Ryan.  I'm currently studying at the UCSD Extension Coding Bootcamp to learn coding.  I graduate from that in September 2022.  Welcome to my portfolio page.  I'm a problem solver at heart; whenever I see a problem all I can do is think about ways to solve it.  That's why I got interested in coding.  It's so much fun.  I get to build super interesting and challenging websites and tools.
+                    Hello!  My name is David Ryan.  Welcome to my portfolio page.  I'm a problem solver at heart; whenever I see a problem all I can do is think about ways to solve it.  That's why I got interested in coding.  It's so much fun.  I get to build super interesting and challenging apps and tools.
                 </p>
                 <br/>
                 <p>
-                    Here you'll have access to see some of my projects as well as info about how to contact me.  I'm a very responsive person when to comes to contacting me.  I check my email roughly twice a day at minimum and I answer texts as soon as I read them (usually within an hour or two).  I do not answer phone calls from unknown numbers unless I'm expecting a call.  Feel free to reach out to me whenever.
+                    I've just graduated from the UCSD Extension Full Stack Flex Coding Bootcamp - I'm now a full stack web developer with deployed projects using tools like React and several different database types.
                 </p>
                 <br/>
                 <p>
-                    I'm have experience writing code in JavaScript, HTML, CSS, and SQL.  I have deployed projects using several different databases (SQL, MongoDB, indexedDB, GraphQL) and tools such as Express, Sequelize, and Mongoose.  I worked on a team of 4-5 on 2 projects so far (1 more to come during bootcamp) one of which is deployed and shown below.  I've also deployed projects using tools such as Handlebars and (more importantly) React.
+                    I'm have experience writing code in JavaScript, HTML, CSS, and SQL.  I have deployed projects using several different databases (SQL, MongoDB, indexedDB, GraphQL) and tools such as Express, Sequelize, and Mongoose.  I have multiple projects built using React and two built with Handlebars (React is my preference of the two by far).  I worked on a team of 4-5 on 3 projects, two of which are deployed and showcased in the Portfolio section of this site.
                 </p>
                 <br/>
                 <p>
