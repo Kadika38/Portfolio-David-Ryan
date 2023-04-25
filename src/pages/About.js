f5441ba50922f1ef3665f6cac67e1d6a92b8dfd1
David <dryan10101@gmail.com>
2023-04-25 11:11:51 -0700

    Updated the wording in the About page

--- src/pages/About.js
+++ src/pages/About.js
@@ -12,23 +12,23 @@ export default function About() {
                 <br/>
                 <br/>
                 <p>
-                    Hello!  My name is David Ryan.  Welcome to my portfolio page.  I'm a problem solver at heart; whenever I see a problem all I can do is think about ways to solve it.  That's why I got interested in coding.  It's so much fun.  I get to build super interesting and challenging apps and tools.
+                    Hello!  My name is David Ryan.  Welcome to my portfolio page!
                 </p>
                 <br/>
                 <p>
-                    I've just graduated from the UCSD Extension Full Stack Flex Coding Bootcamp - I'm now a full stack web developer with deployed projects using tools like React and several different database types.
+                    I graduated from the UCSD Extension Full Stack Flex Coding Bootcamp in 2022.  I'm a full stack web developer with deployed projects using tools like React and several different database types.
                 </p>
                 <br/>
                 <p>
-                    I have experience writing code in JavaScript, HTML, CSS, and SQL.  I have deployed projects using several different databases (SQL, MongoDB, indexedDB, GraphQL) and tools such as Express, Sequelize, and Mongoose.  I have multiple projects built using React and two built with Handlebars (React is my preference of the two by far).  I worked on a team of 4-5 on 3 projects, two of which are deployed and showcased in the Portfolio section of this site.
+                    I have experience writing code in JavaScript, Java, Python, C++, HTML, CSS, and SQL as well as a full understanding of OOP.  I have deployed projects using several different databases (SQL, MongoDB, indexedDB, GraphQL) and tools such as Express, Sequelize, and Mongoose.  I have multiple projects built using React.  I worked on a team of 4-5 on 3 projects, two of which are deployed and one of which is showcased in the Portfolio section of this site.
                 </p>
                 <br/>
                 <p>
-                    Outside of work I enjoy driving cars, exploring San Diego's restaurants, wineries, and breweries, reading, playing video games, seeing movies, and spending time with friends and family.
+                    Outside of work I enjoy driving, exploring San Diego's restaurants, wineries, and breweries, reading, playing video games, seeing movies, and spending time with friends and family.
                 </p>
                 <br/>
                 <p>
-                    Peruse my portfolio - if you're interested enough to reach out, email works best.  My email is dryan10101@gmail.com.  My LinkedIn and GitHub are linked to at the bottom of the page.  You can also reach me with the contact form on the Contact page.  Thanks!
+                    Peruse my portfolio - if you're interested enough to reach out, email works best.  My email is dryan10101@gmail.com.  My LinkedIn and GitHub are linked to at the bottom of the page.  I'm looking for work!  While I'm specialized in web development, I'm a fast learner and more than open to other coding-based positions. Thanks!
                 </p>
             </div>
         </section>
