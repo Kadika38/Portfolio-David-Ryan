09a654d924308487a5aed2f55132baed8d42a76a
Kadika <dryan10101@gmail.com>
2023-05-09 14:02:58 -0700

    updated Portfolio page for JSONBucket project

--- src/pages/Portfolio/Portfolio.js
+++ src/pages/Portfolio/Portfolio.js
@@ -10,9 +10,10 @@ export default function Portfolio() {
                 </h3>
             </div>
             <div className="flexy fullContainer mainStuffWork">
-                <Project num="1" title="PetConnect 2.0" subtitle="Bootcamp Final Project - MERN Stack" link="https://petconnect-p3.herokuapp.com/" />
-                <Project num="2" title="Book Search Engine" subtitle="My first React app" link="https://book-search-engine-david-ryan.herokuapp.com/" />
-                <Project num="3" title="JATE" subtitle="PWA Text Editor" link="https://jate-david-ryan.herokuapp.com/" />
+                <Project num="1" title="JSONBucket" subtitle="JSON to Java Object conversion" link="https://github.com/Kadika38/JSONBucket" />
+                <Project num="2" title="PetConnect 2.0" subtitle="Bootcamp Final Project - MERN Stack" link="https://petconnect-p3.herokuapp.com/" />
+                <Project num="3" title="Book Search Engine" subtitle="My first React app" link="https://book-search-engine-david-ryan.herokuapp.com/" />
+                <Project num="4" title="JATE" subtitle="PWA Text Editor" link="https://jate-david-ryan.herokuapp.com/" />
                 <Project num="5" title="Social Network API" subtitle="Back-end only exemplifying MongoDB and Mongoose" link="https://github.com/Kadika38/Social-Network-API-David-Ryan" />
             </div>
         </section>
