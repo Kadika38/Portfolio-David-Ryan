c76fb2ac262aa08be19c752430828077e3ce1db5
Kadika <dryan10101@gmail.com>
2023-05-09 14:18:46 -0700

    Updated Resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -11,26 +11,52 @@ export default function Resume() {
                 <div className='resumeSpacerBig'></div>
                 <div className='fullContainer'>
                     <p>
-                        Download my <a id='resumeLink' href='../assets/Resume.pdf' download target="_blank">resume</a></p>
+                        See my <a id='resumeLink' href='https://docs.google.com/document/d/11Uazgf6W9LWKCMOCNfqiO3cZmumOIGHIvE9_XoUa0UA/edit?usp=sharing' target="_blank">resume</a></p>
                 </div>
                 <br></br>
                 <div className='resumeSpacer'></div>
                 <div className='fullContainer'>
-                    <h4 className='resumeTitle'>Front-end Proficiencies</h4>
+                    <h4 className='resumeTitle'>Languages</h4>
                     <ul className='resumeList'>
-                        <li>HTML</li>
-                        <li>CSS</li>
                         <li>JavaScript</li>
+                        <li>Java</li>
+                        <li>Python</li>
+                        <li>C++</li>
+                        <li>CSS</li>
+                        <li>HTML</li>
+                        <li>SQL</li>
                     </ul>
                 </div>
                 <br></br>
                 <div className='resumeSpacer'></div>
                 <div className='fullContainer'>
-                    <h4 className='resumeTitle'>Back-end Proficiencies</h4>
+                    <h4 className='resumeTitle'>Proficiencies</h4>
                     <ul className='resumeList'>
+                        <li>React</li>
                         <li>APIs</li>
-                        <li>Node</li>
+                        <li>AWS</li>
+                        <li>MongoDB</li>
+                        <li>GraphQL</li>
+                        <li>Mongoose</li>
+                        <li>Sequelize</li>
                         <li>Express</li>
+                        <li>Node</li>
+                        <li>MERN</li>
+                        <li>Debugging</li>
+                    </ul>
+                </div>
+                <br></br>
+                <div className='resumeSpacer'></div>
+                <div className='fullContainer'>
+                    <h4 className='resumeTitle'>Soft Skills</h4>
+                    <ul className='resumeList'>
+                        <li>Teamwork</li>
+                        <li>Communication</li>
+                        <li>Always Learning</li>
+                        <li>Quick to Adapt</li>
+                        <li>Can Do Attitude</li>
+                        <li>Problem Solving</li>
+                        <li>Creativity</li>
                     </ul>
                 </div>
 
